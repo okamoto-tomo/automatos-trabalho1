1821b01f3138e9f6b5aeb1557fa8f27f6105e957
pedro thiago <pedropedrinhothiago410@gmail.com>
2026-05-01 23:37:21 -0300

    maneira mais eficiente de ler e guardar os dados, além da implementação pra mostrar os 100 primeiros chamados

--- leitura_e_inspecao.py
+++ leitura_e_inspecao.py
@@ -2,13 +2,12 @@
 ### em tese, fazer nesse arquivo só pra leitura dos 4 que o reginaldo mandou
 
 def ler_arquivo_atendimentos():   #função pra ler o arquivo
-    contador = 0   #contaodor pra saber o numero de linhas do arquivo de atendimentos
-    conteudo = ""  #guarda o texto de cada linha do arquivo como uma string e vai interando pelo loop for          
     with open ("automatos-trabalho1/assets/01_atendimentos_bagunçados.txt", "r") as arq :   #lê o arquivo. já tá definido pro modo leitura apenas e deixei o caminho salvo pra ler da pasta do repositorio e da pasta que tá salvo os assets
-        for linha in arq:   #pra interar linha a linha do arquivo
-            contador += 1   #vai somando o numero de linhas
-            conteudo += linha   #vai colocando o texto de cada linha na variavel conteudo    
-        print (contador)   #mostra o numero de linhas do arquivo de atendimentos
+        conteudo = arq.readlines()   #o readlines cria uma lista de strings com base no /n invisivel que tem entre um chamado e outro, e com isso, separa chamado a chamado            
+        contador = len(conteudo)   #se baseia na variavel acima pra decretar o numero de linhas, já que a função de cima lê todo o arquivo antes de "arrumar"  
+        print (f"linhas: {contador}")  #mostra o numero de linhas do arquivo de atendimentos
+        for linha in conteudo [0:100]: #mostra os 100 primeiros chamados 
+            print(linha.strip())   #essa função tira o /n invisivel do final de cada chamado, pra na hora de vizualizar, não ter um espaço extra separando cada chamado
         return contador, conteudo   #retorna os valores de linhas e o texto como um todo pra serem usados depois
 
 
