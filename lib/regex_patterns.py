6a252329798df4f0f521f85023a95e56086c22ee
okamoto <123403861+okamoto-tomo@users.noreply.github.com>
2026-05-21 21:49:18 -0300

    fix: adicionado grupos de não captura (?:) nas expressoes regulares

--- lib/regex_patterns.py
+++ lib/regex_patterns.py
@@ -13,70 +13,70 @@
 
 regex = {
     "email": (
-        r"(?=[a-z0-9\.]{6,30}@)"            # Lookahead necessário para contar o tamanho do local (mínimo 6, máximo 30 char).
-        r"[a-z0-9]"                         # Começa com letra minúscula ou número
-        r"([a-z0-9]|\.[a-z0-9])*"           # Evita pontos seguidos (ex: joao..1@gmail.com é inválido)
-        r"@"                                # Arroba
-        r"([a-z0-9]{2,}\.)+"                # Mínimo de 2 caracteres alfanuméricos seguido de um ponto, no mínimo uma ocorrência disso
-        r"[a-z]{2,}"                        # Top-level domain, no mínimo duas letras minúsculas no final do email
+        r"(?=[a-z0-9\.]{6,30}@)"    # Lookahead necessário para contar o tamanho do local (mínimo 6, máximo 30 char).
+        r"[a-z0-9]"                 # Começa com letra minúscula ou número
+        r"(?:[a-z0-9]|\.[a-z0-9])*" # Evita pontos seguidos (ex: joao..1@gmail.com é inválido)
+        r"@"                        # Arroba
+        r"(?:[a-z0-9]{2,}\.)+"      # Mínimo de 2 caracteres alfanuméricos seguido de um ponto, no mínimo uma ocorrência disso
+        r"[a-z]{2,}"                # Top-level domain, no mínimo duas letras minúsculas no final do email
     ),
     
     "telefone": (
-        r"(\d{2}|\(\d{2}\)|\d{2}\s|\(\d{2}\)\s)?"   # DDD: XX ou (XX) opcional, separado opcionalmente por espaço
+        r"(?:\d{2}|\(\d{2}\)|\d{2}\s|\(\d{2}\)\s)?" # DDD: XX ou (XX) opcional, separado opcionalmente por espaço
         r"9\d{4}"                                   # 9 + primeiros 4 dígitos do número
         r"-?"                                       # Hífen opcional
         r"\d{4}"                                    # Últimos 4 dígitos do número
     ),
     
     "cpf": (
-        r"\d{3}\.\d{3}\.\d{3}-\d{2}"        # CPF com ponto e hífen
+        r"\d{3}\.\d{3}\.\d{3}-\d{2}"    # CPF com ponto e hífen
     ),
     
     "data": (
-        r"(0[1-9]|[1-2]\d|3[0-1])"          # Dia
-        r"/"                                # Barra
-        r"(0[1-9]|1[0-2])"                  # Mês
-        r"/"                                # Barra
-        r"([1-9]\d{3})"                     # Ano
+        r"(?:0[1-9]|[1-2]\d|3[0-1])"    # Dia
+        r"/"                            # Barra
+        r"(?:0[1-9]|1[0-2])"            # Mês
+        r"/"                            # Barra
+        r"(?:[1-9]\d{3})"               # Ano
     ),
     
     "horario": (
-        r"([0-1]\d|2[0-3])"                 # Hora
-        r":"                                # Dois pontos
-        r"[0-5]\d"                          # Minuto
-        r":"                                # Dois pontos
-        r"[0-5]\d"                          # Segundo
+        r"(?:[0-1]\d|2[0-3])"   # Hora
+        r":"                    # Dois pontos
+        r"[0-5]\d"              # Minuto
+        r":"                    # Dois pontos
+        r"[0-5]\d"              # Segundo
     ),
     
     "data_e_horario": (
-        r"(0[1-9]|[1-2]\d|3[0-1])"          # Dia
-        r"/"                                # Barra
-        r"(0[1-9]|1[0-2])"                  # Mês
-        r"/"                                # Barra
-        r"([1-9]\d{3})"                     # Ano
-        r"\s"                               # Espaço
-        r"([0-1]\d|2[0-3])"                 # Hora
-        r":"                                # Dois pontos
-        r"[0-5]\d"                          # Minuto
-        r":"                                # Dois pontos
-        r"[0-5]\d"                          # Segundo
+        r"(?:0[1-9]|[1-2]\d|3[0-1])"    # Dia
+        r"/"                            # Barra
+        r"(?:0[1-9]|1[0-2])"            # Mês
+        r"/"                            # Barra
+        r"(?:[1-9]\d{3})"               # Ano
+        r"\s"                           # Espaço
+        r"(?:[0-1]\d|2[0-3])"           # Hora
+        r":"                            # Dois pontos
+        r"[0-5]\d"                      # Minuto
+        r":"                            # Dois pontos
+        r"[0-5]\d"                      # Segundo
     ),
     
     "url": (
-        r"(https?://)?"                     # Protocolo HTTP ou HTTPS opcional
-        r"(www\.)?"                         # World Wide Web opcional
-        r"([a-z0-9]{2,}\.)+"                # Mínimo de 2 caracteres alfanuméricos seguido de um ponto, no mínimo uma ocorrência disso
-        r"[a-z]{2,}"                        # Top-level domain, no mínimo duas letras minúsculas no final do URL
-        r"(/.*)?"                           # Barra seguido de qualquer coisa de qualquer tamanho, opcional
+        r"(?:https?://)?"       # Protocolo HTTP ou HTTPS opcional
+        r"(?:www\.)?"           # World Wide Web opcional
+        r"(?:[a-z0-9]{2,}\.)+"  # Mínimo de 2 caracteres alfanuméricos seguido de um ponto, no mínimo uma ocorrência disso
+        r"[a-z]{2,}"            # Top-level domain, no mínimo duas letras minúsculas no final do URL
+        r"(?:/.*)?"             # Barra seguido de qualquer coisa de qualquer tamanho, opcional
     ),
     
     "dinheiro": (
-        r"R\$"                              # Real cifrão com espaço opcional
-        r"\s?"                              # Espaço opcional
-        r"((\d{1,3}(\.\d{3})*)"             # 1 a 3 dígitos seguido de zero ou mais ponto com 3 dígitos
-        r"|"                                # OU
-        r"(\d+))"                           # 1 ou mais dígitos
-        r"(,\d{2})?"                        # Vírgula com 2 dígitos, opcional
+        r"R\$"                          # Real cifrão com espaço opcional
+        r"\s?"                          # Espaço opcional
+        r"(?:(?:\d{1,3}(?:\.\d{3})*)"   # 1 a 3 dígitos seguido de zero ou mais ponto com 3 dígitos
+        r"|"                            # OU
+        r"(?:\d+))"                     # 1 ou mais dígitos
+        r"(?:,\d{2})?"                  # Vírgula com 2 dígitos, opcional
     ),
     
     "nome": (
@@ -105,4 +105,10 @@ Observação 2:
 Sem formatação, é ambíguo extrair telefone e CPF por ambos poderem possuir 11 dígitos.
 Dessa forma, decidiu-se que o CPF deve obrigatoriamente estar formatado, enquanto que
 a formatação do telefone é opcional.
+
+
+
+Observação 3:
+Grupo de não-captura (?:) diz respeito à forma de como o motor de busca casa os valores
+e nada interfere no poder computacional, continua sendo Linguagem Regular (tipo 3).
 '''
