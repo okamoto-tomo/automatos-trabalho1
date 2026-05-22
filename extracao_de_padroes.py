ec63a904faaaeedb5dcda4be24840e820e61d3e0
okamoto <123403861+okamoto-tomo@users.noreply.github.com>
2026-05-21 21:56:03 -0300

    add: extraçao de padrões

--- extracao_de_padroes.py
+++ extracao_de_padroes.py
@@ -1,1 +1,25 @@
 ### implementação com expressão regular pra identificar e extrair os dados 
+
+# falta expressoes_regulares
+import leitura_e_inspecao
+import lib.regex_patterns
+import re
+
+def extrair_valores(caminho: str, expressoes_regulares: dict) -> list[tuple[str, str, str]]:
+    lista = []
+    
+    arquivo_origem = caminho.replace("assets/", "")
+    arquivo = leitura_e_inspecao.ler_arquivo(caminho)[1][:10]
+    
+    for chave in expressoes_regulares.keys():
+        tipo = chave
+        
+        for linha in arquivo:
+            expressao_regular = expressoes_regulares[tipo]
+            matches = re.findall(expressao_regular, linha)
+            
+            if matches:
+                for valor in matches:
+                    lista.append((tipo, valor, arquivo_origem))
+                    
+    return lista
