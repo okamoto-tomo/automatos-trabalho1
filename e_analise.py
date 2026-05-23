51bb9cb63d366e6b96e55c8f5aaf79bb8f5b0173
okamoto <123403861+okamoto-tomo@users.noreply.github.com>
2026-05-23 12:01:14 -0300

    init

--- e_analise.py
+++ e_analise.py
@@ -1,1 +1,7 @@
-### estatisticas sobre os dados extraidos 
+'''
+e) Análise quantitativa. 
+O sistema deve produzir estatísticas sobre os dados extraídos, incluindo, no mínimo: 
+quantidade total de ocorrências por tipo; quantidade de ocorrências válidas e 
+inválidas por tipo; distribuição das ocorrências entre os arquivos.
+'''
+
